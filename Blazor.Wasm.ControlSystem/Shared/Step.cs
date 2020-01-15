db68340994cc71bf378eae0d7cf7994dd7b488b7
Jeff Barnard <jeffbarnard@gmail.com>
2020-01-14 22:53:33 -0500

    Introduce change detection for complex type StepState

--- Blazor.Wasm.ControlSystem/Shared/Step.cs
+++ Blazor.Wasm.ControlSystem/Shared/Step.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Blazor.Wasm.ControlSystem.Shared
 {
+    /// <summary>
+    /// A process step.
+    /// Steps are completed when all of their operations are completed.
+    /// </summary>
     public class Step
     {
+        public Action ValueChanged { get; set; }
+
         public int StepId { get; set; }
         public string Name { get; set; }
-        public StepState State { get; set; }
+
+        private StepState _state = StepState.NotSet;
+        public StepState State
+        {
+            get => _state;
+            set
+            {
+                _state = value;
+                ValueChanged?.Invoke();
+            }
+        }
+        public bool IsComplete => State == StepState.Completed;
 
         public List<Operation> Operations { get; set; }
+
+        internal void OnValueChanged()
+        {
+            State = StepState.Completed;
+        }
     }
 
     public enum StepState : int
---
+++ Blazor.Wasm.ControlSystem/Shared/StepController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Wasm.ControlSystem.Shared
+{
+    public class StepController
+    {
+        public List<Step> Steps { get; set; }
+
+        public void Initialize()
+        {
+            // wire up Action delegates
+            Steps.ForEach(s => s.Operations.ForEach(o => o.ValueChanged = s.OnValueChanged));
+        }
+        
+    }
+}
